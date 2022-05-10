49bd53e8c1ebb2ea476153513cdc087967f4340b
Stanislav Sava <sava.stanislav@gmail.com>
2022-05-10 14:17:43 +0300

    rename import type to simple import

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import type { Frame } from 'react-native-vision-camera';
+import { Frame } from 'react-native-vision-camera';
 
 /**
  * @see https://developers.google.com/android/reference/com/google/mlkit/vision/barcode/Barcode.BarcodeFormat
